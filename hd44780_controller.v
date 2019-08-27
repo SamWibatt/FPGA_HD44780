6de8d6f3ff443c28ca874846517b73a5439f608a
Sean Igo <samwibatt@gmail.com>
2019-08-26 20:22:56 -0700

    roughing state machines

--- hd44780_controller.v
+++ hd44780_controller.v
@@ -73,7 +73,7 @@ module state_timer #(parameter SYSFREQ = `G_SYSFREQ, parameter STATE_TIMER_BITS 
     input wire CLK_I,
 	input wire [STATE_TIMER_BITS-1:0] DAT_I,	//[STATE_TIMER_BITS-1:0] DAT_I,
     input wire start_strobe,            // causes timer to load
-    output wire end_strobe              // nudges caller to advance state
+    output wire end_strobe             // nudges caller to advance state
     );
 
     // DEBUG ===============================================================================
@@ -117,27 +117,60 @@ module state_timer #(parameter SYSFREQ = `G_SYSFREQ, parameter STATE_TIMER_BITS 
 
 endmodule
 
+module nybble_sender(
+    input RST_I,                    //wishbone reset, also on falling edge of reset we want to do the whole big LCD init.
+    input CLK_I,
+    input STB_I,                    //to let this module know rs and lcd_data are ready and to do its thing.
+    input i_rs,                     //register select - command or data, will go to LCD RS pin
+    input wire[3:0] nybble,         //nybble we're sending
+    output wire busy,               //whether this module is busy
+    output wire [7:4] o_lcd_data,   //can you do this? the data bits we send really are 7:4 - I guess others NC? tied low?
+    output wire o_rs,
+    output wire o_e                   //LCD enable pin
+    );
 
-//***********************************************************************************************************
-//***********************************************************************************************************
-//***********************************************************************************************************
-// HEREAFTER UNCHANGED **************************************************************************************
-//***********************************************************************************************************
-//***********************************************************************************************************
-//***********************************************************************************************************
+    always @(posedge CLK_I) begin
+        if(RST_I) begin
+            //reset!
+        end else if (STB_I & ~busy) begin
+            //strobe came along while we're not busy! let's get rolling
+        end else begin
+            // our little state machine! All of the following with the appropriate delays
+            // ************************* FIND OUT WHAT THE DELAYS ARE from hd44780 'sheet
+            // put the nybble on the output lines
+            // set r/s appropriately
+            // - wait for TAS (address setup time), 60ns (pg 49 of 'sheet)
+            // - here is where TCYCE starts
+            // raise e
+            // - wait for PWEH (pulse width enable high), 450 ns min
+            // lower e
+            // - wait for TAH (address hold time), 20 ns
+            // - wait for rest of TCYCE (Enable cycle time) s.t. to here it's 1000 ns, so
+            //     1000 - (TAS + TAH + PWEH) = 1000 - (60 + 450 + 20) = 1000 - 530 = 470 ?
+            // - now we can leave, I reckon
+        end
+    end
 
+
+endmodule
 
 // MAIN ********************************************************************************************************************************************
 module hd44780_controller(
-    input RST_I,                //wishbone reset, also on falling edge of reset we want to do the whole big LCD init.
+    input RST_I,                    //wishbone reset, also on falling edge of reset we want to do the whole big LCD init.
     input CLK_I,
-    input STB_I,                //to let this module know rs and lcd_data are ready and to do its thing.
-    input rs,                   //register select - command or data, will go to LCD RS pin
-    input wire[7:0] lcd_data,   // byte to send to LCD, one nybble at a time
-    output busy,
-	output alive_led,			//this is THE LED, the green one that shows the controller is alive
+    input STB_I,                    //to let this module know rs and lcd_data are ready and to do its thing.
+    input i_rs,                     //register select - command or data, will go to LCD RS pin
+    input wire[7:0] i_lcd_data,     // byte to send to LCD, one nybble at a time
+    output wire busy,
+	output wire alive_led,			//this is THE LED, the green one that shows the controller is alive
+    output wire o_rs,
+    output wire [7:4] o_lcd_data,   //can you do this? the data bits we send really are 7:4 - I guess others NC? tied low?
+    output wire o_e                 //LCD enable pin
 );
 
+    reg busy_reg = 0;               //for synching busy flag
+    reg active = 0;                 //0 means in reset so post-reset can spot
+    reg [3:0] nybble = 0;           //nybble to send to LCD
 
 	// Super simple "I'm Alive" blinky on one of the external LEDs.
 	parameter GREENBLINKBITS = 25;			// at 12 MHz 23 is ok - it's kind of hyper at 48. KEY THIS TO GLOBAL SYSTEM CLOCK FREQ DEFINE
@@ -170,26 +203,137 @@ module hd44780_controller(
 		.end_strobe(timer_done)
 	);
 
-    reg in_reset_n = 0;                 //0 means in reset so post-reset can spot
+    //state-machiney stuff
+    //let's not bother with my old-fashioned single-bit transitions...?
+    //remember a state machine can just increment through states or have defines or whatever
+    //it's also possible to have unrelated bits of state machine - think of it like BASIC line numbers,
+    //there could be a set of states reset_lcd_0, reset_lcd_1, ... reset_lcd_n s.t. those are the
+    //states that walk through the LCD reset and then release busy and "hang" in a main state that
+    //does nothing - STB_I strobe or another reset will jolt it into the new state it needs to be in.
+    //Then there's another set of states like send_char_or_cmd_0, ... send_char_or_cmd_n, that does the
+    //stuff where it presents i_lcd_data on the output pins one nybble at a time, waits for
+    //whatever time, sets rs, raises e, etc. etc. This is where clever muxing or something could
+    //be nice for the presentation of a nybble on the output.
+    //here's how UW https://courses.cs.washington.edu/courses/cse370 does it
+    //localparam IDLE=0, WAITFORB=1, DONE=2, ERROR=3;
+    /* Here is the initialization sequence given in http://web.alfredstate.edu/faculty/weimandn/lcd/lcd_initialization/lcd_initialization_index.html
+    Copyright © 2009, 2010, 2012 Donald Weiman
+    (weimandn@alfredstate.edu)
+
+    Step 1. Power on, then delay > 100 ms
+    There are two different values (with two different references) specified on the datasheet flowchart for this initial delay but neither
+    reference is from when the power was first applied. The delay required from power-up must obviously be more than 40 mS and I
+    have arbitrarily chosen to use 100 mS. Since this delay only occurs once it doesen't make sense to try to speed up program
+    execution time by skimping on this delay.
+
+    Step 2. Instruction 0011b (3h), then delay > 4.1 ms
+    This is a special case of the Function Set instruction where the lower four bits are irrelevant. These four bits are not shown on
+    the flowcharts because the host microcontroller does not usually implement them at all (as opposed to the 8-bit mode wher they
+    are implemented as '0's).
+    This first instruction, for some unexplained reason, takes significantly longer to complete than the ones
+    that come later.
+
+    Step 3. Instruction 0011b (3h), then delay > 100 us
+    This is a second instance of the special case of the Function Set instruction. The controller does not normally expect to receive
+    more than one 'Function Set' instruction so this may account for the longer than normal execution time.
+
+    Step 4. Instruction 0011b (3h), then delay > 100 us
+    This is a third instance of the special case of the Function Set instruction.
+    By now the LCD controller realizes that what is really
+    intended is a 'reset', and it is now ready for the real Function Set instruction followed by the rest of the initialization instructions.
+    The flowcharts do not specify what time delay belongs here.
+    I have chosen 100 us to agree with the previous instruction.
+    It may be possible to check the busy flag here.
+
+    Step 5. Instruction 0010b (2h), then delay > 100 us
+    Here is where the LCD controller is expecting the 'real' Function Set instruction which, in the 8-bit mode, would start with 0011.
+    Instead, it gets a Function Set instruction starting with 0010. This is it's signal to again ignore the lower four bits, switch to the
+    four-bit mode, and expect another 'real' Function Set instruction. Once again the required time delay is speculation.
+    The LCD controller is now in the 4-bit mode. This means that the LCD controller reads only the four high order data pins each
+    time the Enable pin is pulsed . To accomodate this, the host microcontroller must put the four high bits on the data lines and
+    pulse the enable pin, it must then put the four low bits on the data lines and again pulse the enable pin. There is no need for a
+    delay between these two sequences because the LCD controller isn't processing the instruction yet. After the second group of
+    data bits is received the LCD controller reconstructs and executes the instruction and this is when the delay is required.
+
+    Step 6. Instruction 0010b (2h), then 1000b (8h), then delay > 53 us or check BF
+    This is the real Function Set instruction. This is where the interface, the number of lines, and the font are specified. Since we
+    are implementing the 4-bit interface we make D = 0. The number of lines being specified here is the number of 'logical' lines as
+    perceived by the LCD controller, it is NOT the number of 'physical' lines (or rows) that appear on the actual display. This should
+    almost always be two lines so we set N=1 (go figure). There are very few displays capable of displaying a 5x10 font so the 5x7
+    choice is almost always correct and we set F=0.
+
+    Step 7. Instruction 0000b (0h), then 1000b (8h) then delay > 53 us or check BF
+    This is the Display on/off Control instruction. This instruction is used to control several aspects of the display but now is NOT
+    the time to set the display up the way we want it. The flow chart shows the instruction as 00001000, not 00001DCB which
+    indicates that the Display (D), the Cursor (C), and the Blinking (B) should all be turned off by making the corresponding bits = 0.
+
+    Step 8. Instruction 0000b (0h), then 0001b (1h) then delay > 3 ms or check BF
+    This is the Clear Display instruction which, since it has to write information to all 80 DDRAM addresses, takes more time to
+    execute than most of the other instructions. On some flow charts the comment is incorrectly labeled as 'Display on' but the
+    instruction itself is correct.
+
+    Step 9. Instruction 0000b (0h), then 0110b (6h), then delay > 53 us or check BF
+    This is the Entry Mode Set instruction. This instruction determines which way the cursor and/or the display moves when we
+    enter a string of characters. We normally want the cursor to increment (move from left to right) and the display to not shift so
+    we set I/D=1 and S=0. If your application requires a different configuration you could change this instruction, but my
+    recommendation is to leave this instruction alone and just add another Entry Mode Set instruction where appropriate in your
+    program.
+
+    Step 10. Initialization ends
+    This is the end of the actual intitalization sequence, but note that step 6 has left the display off.
+
+    Step 11. Instruction 0000b (0h), then 1100b (0Ch), then delay > 53 us or check BF
+    This is another Display on/off Control instruction where the display is turned on and where the cursor can be made visible
+    and/or the cursor location can be made to blink. This example shows the the display on and the other two options off, D=1, C=0,
+    and B=0.
+
+    ... This COULD maybe be done as a little table of nybble, delay - but let's do a state machine
+    */
+
+    // **********************************************
+    // SO HERE IS THE LIST OF STATES WE'LL BE DEALING WITH IN THIS MODULE -
+    // THIS WILL AMOUNT TO A BUNCH OF USES OF THE TIMER MODULE AND THE NYBBLE SENDER MODULE
+    localparam IDLE=0,              // state where we end up after init or char/cmd send
+        RESET_LCD_START = 1,        // Step 1. Power on, then delay > 100 ms
+        RESET_LCD_FNSET1 = 2,       // Step 2. Instruction 0011b (3h), then delay > 4.1 ms
+        RESET_LCD_FNSET2 = 3,       // Step 3. Instruction 0011b (3h), then delay > 100 us
+        RESET_LCD_FNSET3 = 4,       // Step 4. Instruction 0011b (3h), then delay > 100 us
+        RESET_LCD_4BIT = 5,         // Step 5. Instruction 0010b (2h), then delay > 100 us
+        RESET_LCD_FS4LNFNT = 6,     // Step 6. Instruction 0010b (2h), then 1000b (8h), then delay > 53 us or check BF
+        RESET_LCD_DISPOFF = 7,      // Step 7. Instruction 0000b (0h), then 1000b (8h) then delay > 53 us or check BF
+        RESET_LCD_CLRDISP = 8,      // Step 8. Instruction 0000b (0h), then 0001b (1h) then delay > 3 ms or check BF
+        RESET_LCD_EMSET = 9,        // Step 9. Instruction 0000b (0h), then 0110b (6h), then delay > 53 us or check BF
+                                    // Step 10. Initialization ends
+        RESET_LCD_DISPON = 10,      // Step 11. Instruction 0000b (0h), then 1100b (0Ch), then delay > 53 us or check BF
+        // [...]
+        SENDCHAR_START = ,
+
+
+
     always @(posedge CLK_I) begin
         if(RST_I) begin
-            //glue stuff down
-            //maybe set some register so we know we've been in reset, let's say
-            //in_reset_n = 0 when we're in reset
-            in_reset_n <= 0;
+            //in reset, zero everything out
+            greenblinkct <= 0;
+            busy_reg <= 0;
+            timer_start <= 0;
+            timer_value <= 0;
+            active <= 0;            // positive edge in "active" shows reset is newly done so init LCD
         end else begin
-            //need a bit in here somewhere about how if reset is freshly released
-            //the in_reset register
-            if(~in_reset_n) begin
-                //send busy and reset LCD 
-                in_reset_n <= 1;            //dismiss just-reset active low flag
-            end else if (STB_I) begin
-                //aha, raise the
+            if(~active) begin
+                //aha, we've just exited reset. send busy and reset LCD
+                busy_reg <= 1;
+                active <= 1;            //dismiss just-reset condition: now active
+                //************ GO OFF AND DO RESET - HOW? Big state machine below?
+            end else if (STB_I & ~busy) begin
+                //Strobe doesn't do anything if we're already busy
+                //aha, raise the busy flag and do whatever it is
+                busy_reg <= 1;
+                // ***************** MORE STUFF
             end else begin
-                //state-machiney stuff
+                //state machine!
             end
         end
     end
-
+    assign busy = busy_reg;
 
 endmodule
